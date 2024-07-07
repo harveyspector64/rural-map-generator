21f4b05b5468a4df8c7b60bbd2b277b26ffb030d
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 18:15:49 -0700

    Update TileConfig.js

--- src/config/TileConfig.js
+++ src/config/TileConfig.js
@@ -1,1 +1,25 @@
+// TileConfig.js
 
+export const TILE_TYPES = [
+  'grass', 'field', 'dirt', 'road', 'tree', 'bushyGrass', 'water'
+];
+
+export const VALID_NEIGHBORS = {
+  grass: ['grass', 'field', 'dirt', 'road', 'tree', 'bushyGrass', 'water'],
+  field: ['field', 'dirt', 'road'],
+  dirt: ['grass', 'field', 'dirt', 'road'],
+  road: ['grass', 'field', 'dirt', 'road'],
+  tree: ['grass', 'bushyGrass'],
+  bushyGrass: ['grass', 'tree', 'bushyGrass'],
+  water: ['grass', 'water']
+};
+
+export const TILE_WEIGHTS = {
+  grass: 10,
+  field: 5,
+  dirt: 3,
+  road: 1,
+  tree: 2,
+  bushyGrass: 2,
+  water: 1
+};
