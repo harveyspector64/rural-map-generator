32a61f61df03bad5605635a15f41414bb728f7c8
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 18:31:34 -0700

    Create RandomUtils.js

---
+++ src/utils/RandomUtils.js
@@ -0,0 +1,26 @@
+// src/utils/RandomUtils.js
+
+class RandomUtils {
+  static shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  static weightedChoice(items, weights) {
+    const totalWeight = weights.reduce((sum, weight) => sum + weight, 0);
+    let random = Math.random() * totalWeight;
+    
+    for (let i = 0; i < items.length; i++) {
+      random -= weights[i];
+      if (random <= 0) {
+        return items[i];
+      }
+    }
+    return items[items.length - 1];
+  }
+}
+
+export default RandomUtils;
